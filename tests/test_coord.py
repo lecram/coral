d0b49dfc3a0f6afae68b07bbea06369893dad989
Marcel Rodrigues <marcelgmr@gmail.com>
2014-06-12 11:42:39 -0300

    Update test for simplify().

--- tests/test_coord.py
+++ tests/test_coord.py
@@ -5,10 +5,10 @@ from coral import coord
 class TestCoord(unittest.TestCase):
 
     def test_simplify(self):
-        poly = [(-10, -10), (-10, 10), (0, 15), (10, 10), (10, -10), (0, -5)]
-        poly = coord.simplify(poly, 10)
+        poly = [(-10, -10), (-10, 10), (0, 13), (10, 10), (10, -10), (0, -7)]
+        poly = list(coord.simplify(poly, 10))
         simple = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
-        self.assertEqual(poly, simple)
+        self.assertEqual(poly, simple[1:] + simple[:1])
 
 if __name__ == "__main__":
     unittest.main()
