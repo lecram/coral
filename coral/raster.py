b3f125e8956ff53be08a2309a85a69bee4c87407
Marcel Rodrigues <marcelgmr@gmail.com>
2014-07-20 13:46:33 -0300

    Remove PyPNG.

--- coral/raster.py
+++ coral/raster.py
@@ -26,8 +26,6 @@ import math
 import heapq
 from array import array
 
-from . import png
-
 class BaseCanvas:
 
     def scanpolygon(self, points, scans=None):
@@ -190,49 +188,3 @@ class ColorCanvas(BaseCanvas):
             f.write("{0.width} {0.height}\n".format(self).encode("ascii"))
             f.write(b"255\n")
             self.data.tofile(f)
-
-class PixMap:
-
-    def __init__(self, width, height, bg=255):
-        self.width = width
-        self.height = height
-        self.pixmap = [[bg] * width for i in range(height)]
-
-    def splitpolygon(self, points):
-        polys = []
-        poly = []
-        for (x0, y0), (x1, y1) in zip(points, points[1:] + points[:1]):
-            if x1 - x0 > self.width / 2:
-                ym = y0 + round((y1 - y0) * x0 / (x0 + self.width - x1 + 1))
-                poly.extend([(x0, y0), (0, ym)])
-                polys.append(poly)
-                poly = []
-            elif x0 - x1 > self.width / 2:
-                ym = y1 - round((y1 - y0) * x1 / (x1 + self.width - x0 + 1))
-                poly.extend([(x0, y0), (self.width - 1, ym)])
-                polys.append(poly)
-                poly = []
-            else:
-                poly.append((x0, y0))
-        if polys:
-            polys[0] = poly + polys[0]
-        else:
-            polys = [poly]
-        return polys
-
-    def drawpredicate(self, func, color):
-        "Draw pixels for which `func(x, y)` returns True."
-        for y in range(self.height):
-            for x in range(self.width):
-                if func(x, y):
-                    self.pixmap[y][x] = color
-
-    def save(self, fname, reverse=True, **kwargs):
-        writer = png.Writer(self.width, self.height, compression=9, **kwargs)
-        pm = self.pixmap
-        if reverse:
-            pm = pm[::-1]
-        if isinstance(pm[0][0], tuple):
-            pm = [sum(row, ()) for row in pm]
-        with open(fname, "wb") as f:
-            writer.write(f, pm)
