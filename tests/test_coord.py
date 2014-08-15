c7ebd1c1e4916977ab68887d39c044c407aee989
Marcel Rodrigues <marcelgmr@gmail.com>
2014-08-15 14:06:06 -0300

    Remove test for simplify().

--- tests/test_coord.py
+++ tests/test_coord.py
@@ -4,11 +4,5 @@ from coral import coord
 
 class TestCoord(unittest.TestCase):
 
-    def test_simplify(self):
-        poly = [(-10, -10), (-10, 10), (0, 13), (10, 10), (10, -10), (0, -7)]
-        poly = list(coord.simplify(poly, 10))
-        simple = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
-        self.assertEqual(poly, simple[1:] + simple[:1])
-
 if __name__ == "__main__":
     unittest.main()
